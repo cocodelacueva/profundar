7c8d03c1a5824228267f524da1913009e34a2106
coco <cocodelacueva@gmail.com>
2022-02-15 23:14:10 -0300

    camelcase

--- components/post.short.js
+++ components/post.short.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from './styles/PostShort.module.css'
+import styles from './styles/Postshort.module.css'
 
 export default function PostShort(post) {
     //textos
